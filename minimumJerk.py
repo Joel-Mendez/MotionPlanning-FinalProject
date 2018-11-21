bb9c8fa4770fa57e2d399db27d0d0b082f82e3e4
Joel-Mendez <Joel-Mendez@users.noreply.github.com>
2018-11-21 10:15:28 -0700

    Working minimumJerk.py returns coefficients for trajectory.

--- minimumJerk.py
+++ minimumJerk.py
@@ -5,7 +5,6 @@
 # The solution is a 6-D vector of coefficients a
 # The minimum jerk trajectory takes the form x_t = \sum_{k=1}^6 a_k t^(k-1), for 0\leq t \leq T
 import numpy as np
-
 def minimumJerk(x0, dx0, ddx0,xT,dxT,ddxT,T):
 
     T2 = T*T #T squared
@@ -18,22 +17,7 @@ def minimumJerk(x0, dx0, ddx0,xT,dxT,ddxT,T):
     a[1] = dx0
     a[2] = ddx0/2
     b=np.array([ [T3, T4, T5], [3*T2, 4*T3, 5*T4], [6*T, 12*T2, 20*T3] ])
-    c=np.array([ [xT-a[0]-a[1]*T-a[2]*T2] , [dxT-a[1]-2*a[2]*T], [ddxT-2*a[2]] ])
-    print 'b'
-    print b
-    print'c'
-    print c
-    d=np.linalg.pinv(b)
-    print 'd'
-    print d
-    e=np.matmul(d,c)
-    print 'e'
-    print e
-    # a(4:6,1)=pinv(b)*c;
-    a[3:6,:1]=e
+    c=np.array([xT-a[0]-a[1]*T-a[2]*T2, dxT-a[1]-2*a[2]*T, ddxT-2*a[2] ])
+    a[3:6,:1]=np.dot(np.linalg.pinv(b),c)
 
     return a
-
-a=minimumJerk(60,0,0,0,0,0,2)
-
-print a
