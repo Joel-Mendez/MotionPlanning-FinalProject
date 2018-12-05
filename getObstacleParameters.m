57e87cbaea18e9ec256252f4ccc1c3db30118783
Joel-Mendez <45205283+Joel-Mendez@users.noreply.github.com>
2018-12-05 08:26:09 -0700

    Update getObstacleParameters.m

--- getObstacleParameters.m
+++ getObstacleParameters.m
@@ -1,9 +1,9 @@
 function ObstacleParameters = getObstacleParameters(clientID,ObstacleHandle)
 
-[~,zmax]=simxGetObjectFloatParameter(clientID,ObstacleHandle,20,vrep.simx_opmode_blocking); %Not sure abot the opmode
-[~,zmin]=simxGetObjectFloatParameter(clientID,ObstacleHandle,17,vrep.simx_opmode_blocking);
-[~,xmax]=simxGetObjectFloatParameter(clientID,ObstacleHandle,18,vrep.simx_opmode_blocking);
-[~,xmin]=simxGetObjectFloatParameter(clientID,ObstacleHandle,15,vrep.simx_opmode_blocking);
+[~,zmax]=vrep.simxGetObjectFloatParameter(clientID,ObstacleHandle,20,vrep.simx_opmode_blocking); %Not sure abot the opmode
+[~,zmin]=vrep.simxGetObjectFloatParameter(clientID,ObstacleHandle,17,vrep.simx_opmode_blocking);
+[~,xmax]=vrep.simxGetObjectFloatParameter(clientID,ObstacleHandle,18,vrep.simx_opmode_blocking);
+[~,xmin]=vrep.simxGetObjectFloatParameter(clientID,ObstacleHandle,15,vrep.simx_opmode_blocking);
 
 ObstacleHeight=zmax-zmin;
 ObstacleLength=xmax-xmin;
